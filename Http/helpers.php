d8ad800530f1ffd5ddfee772e5146aabcf1ddb99
yusidabcs <agusyusida@gmail.com>
2017-03-08 22:07:40 +0800

    fix validation

--- Http/helpers.php
+++ Http/helpers.php
@@ -1,16 +1,16 @@
 <?php
 
 function get_galleries(){
-	$galleries = Modules\Gallery\Entities\Gallery::all();
+	$galleries = Modules\Gallery\Entities\Gallery::has('files')->get();
 	return $galleries;
 }
 
 function gallery_image($gallery,$size = null){
 
 	if($size == null){
-		return url($gallery->files ? $gallery->files[0]->path : '');	
+		return url($gallery->files ? $gallery->files->first()->path : '');
 	}else{
-		return Imagy::getThumbnail($gallery->files ? $gallery->files[0]->path : '', $size);
+		return Imagy::getThumbnail($gallery->files ? $gallery->first()->path : '', $size);
 	}
 	
 }
